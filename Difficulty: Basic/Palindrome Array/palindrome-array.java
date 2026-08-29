a591d6031fc75fe720fa396b78c7450631b3a218
KARRI DURGA SIVA <durgasivayyakarri@gmail.com>
2026-08-29 21:24:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Palindrome Array/palindrome-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static boolean isPalindrome(int[] arr) {
+        // code here
+        int n=arr.length;
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            if(arr[i]!=arr[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+            
+                return true;
+            }
+        
+         
+    }
