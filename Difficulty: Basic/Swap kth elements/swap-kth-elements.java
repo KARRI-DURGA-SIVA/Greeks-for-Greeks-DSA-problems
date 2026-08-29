d03b2af3df9d7faf05b9cf9af4d0a2d982c5fda0
KARRI DURGA SIVA <durgasivayyakarri@gmail.com>
2026-08-29 21:37:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Swap kth elements/swap-kth-elements.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public void swapKth(List<Integer> arr, int k) {
+        // code here
+        int n=arr.size();
+        int i=k-1;
+        int j=n-k;
+         
+         int temp=arr.get(i);
+         arr.set(i,arr.get(j));
+         arr.set(j,temp);
+    }
+}
