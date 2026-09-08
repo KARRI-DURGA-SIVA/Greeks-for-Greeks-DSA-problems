7ebbb464f35734a8910bee3f5f60d7cd8962050c
KARRI DURGA SIVA <durgasivayyakarri@gmail.com>
2026-09-08 14:32:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse Subarray/reverse-subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static ArrayList<Integer> reverseSubArray(
+            ArrayList<Integer> arr, int l, int r) {
+
+        l = l - 1;
+        r = r - 1;
+
+        while (l < r) {
+            int temp = arr.get(l);
+            arr.set(l, arr.get(r));
+            arr.set(r, temp);
+
+            l++;
+            r--;
+        }
+
+        return arr;
+    }
+}
